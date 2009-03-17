58d6af41f406852cd24963f6a495583513045e14
Eraser <Eraser@0e6492ef-cedd-114b-947f-3769be7ed64a>
2009-03-17 15:57:21 +0000

    Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/
    git-svn-id: file:///e/Backups/vorderklier.de/backup/160817/data/svn/PartyDJ_Alt@104 0e6492ef-cedd-114b-947f-3769be7ed64a

---
+++ src/_test/Zeug.java
@@ -0,0 +1,25 @@
+package _test;
+
+public class Zeug
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		for(int i = 0; i <= 100; i += 10)
+		{
+			System.out.println(i + ": " + f(i));
+		}
+	}
+	
+	static double f(int i)
+	{
+		double min = -80;
+		double max = 6.0206;
+		double fa = 172.17390699942;
+		return Math.log((i + 1) * fa) / Math.log(101 * fa) * (max - min) + min;
+	}
+
+}
