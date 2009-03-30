049fd5ebd697da1a8668cce4bf966163ca63cdef
Sam <Sam@0e6492ef-cedd-114b-947f-3769be7ed64a>
2009-03-30 11:04:04 +0000

    Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/
    git-svn-id: file:///e/Backups/vorderklier.de/backup/160817/data/svn/PartyDJ_Alt@147 0e6492ef-cedd-114b-947f-3769be7ed64a

---
+++ src/gui/dnd/TrackSelection.java
@@ -0,0 +1,34 @@
+package gui.dnd;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+import common.Track;
+
+public class TrackSelection implements Transferable
+{
+	private final Object[] trackArray;
+	private final DataFlavor trackFlavor = new DataFlavor(Track.class, "Track flavor");
+	private final DataFlavor flavors[] = {trackFlavor};
+
+	public TrackSelection(Object[] trackArray)
+	{
+		this.trackArray = trackArray;
+	}
+	
+	public synchronized Object[] getTransferData(DataFlavor dataFlavor) throws UnsupportedFlavorException, IOException
+	{
+		return trackArray;
+	}
+
+	public synchronized DataFlavor[] getTransferDataFlavors()
+	{
+		return flavors;
+	}
+
+	public boolean isDataFlavorSupported(DataFlavor dataFlavor)
+	{
+		return (dataFlavor.equals(trackFlavor));
+	}
+}
