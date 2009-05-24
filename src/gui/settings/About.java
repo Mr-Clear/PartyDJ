202c98071638c2500c53d1f1ab42b400805efe8e
Eraser <Eraser@0e6492ef-cedd-114b-947f-3769be7ed64a>
2009-05-24 17:56:01 +0000

    - invokeLater
    git-svn-id: file:///e/Backups/vorderklier.de/backup/160817/data/svn/PartyDJ_Alt@278 0e6492ef-cedd-114b-947f-3769be7ed64a

--- src/gui/settings/About.java
+++ src/gui/settings/About.java
@@ -5,7 +5,6 @@ import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 import data.SettingException;
 import basics.Controller;
 
@@ -24,51 +23,46 @@ public class About extends JPanel
 	{
 		super();
 		
-		SwingUtilities.invokeLater(new Runnable(){
-			@Override
-			public void run()
-			{
-				setLayout(new BorderLayout());
-				Box box = Box.createVerticalBox();
-				
-				box.add(Box.createVerticalStrut(8));
-				
-				JLabel lbl = null;
-				try
-				{
-					lbl = new JLabel(new ImageIcon("Resources/Schriftzug.png"));
-				}
-				catch (NullPointerException e)
-				{
-					lbl = new JLabel("Hier fehlt ein Bild :(");
-				}
-				lbl.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-				box.add(lbl);
+		setLayout(new BorderLayout());
+		Box box = Box.createVerticalBox();
+		
+		box.add(Box.createVerticalStrut(8));
+		
+		JLabel lbl = null;
+		try
+		{
+			lbl = new JLabel(new ImageIcon("Resources/Schriftzug.png"));
+		}
+		catch (NullPointerException e)
+		{
+			lbl = new JLabel("Hier fehlt ein Bild :(");
+		}
+		lbl.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		box.add(lbl);
 
-				box.add(Box.createVerticalGlue());
-				lbl = new JLabel("PartyDJ Version " + Controller.getInstance().version);
-				lbl.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-				box.add(lbl);
-				
-				box.add(Box.createVerticalStrut(8));
-				lbl = new JLabel("Geschrieben von Thomas Klier und Samantha Vordermeier");
-				lbl.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-				box.add(lbl);
-				
-				box.add(Box.createVerticalStrut(8));
-				lbl = new JLabel("Tracks gesamt: " + Controller.getInstance().getListProvider().getMasterList().getSize());
-				lbl.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-				box.add(lbl);
-				
-				try
-				{
-					lbl = new JLabel("Ladedauer: " + Integer.parseInt(Controller.getInstance().getData().readSetting("LastLoadTime")) / 1000d + "Sekunden");
-					lbl.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-					box.add(lbl);
-				}catch (SettingException e){}
+		box.add(Box.createVerticalGlue());
+		lbl = new JLabel("PartyDJ Version " + Controller.getInstance().version);
+		lbl.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		box.add(lbl);
+		
+		box.add(Box.createVerticalStrut(8));
+		lbl = new JLabel("Geschrieben von Thomas Klier und Samantha Vordermeier");
+		lbl.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		box.add(lbl);
+		
+		box.add(Box.createVerticalStrut(8));
+		lbl = new JLabel("Tracks gesamt: " + Controller.getInstance().getListProvider().getMasterList().getSize());
+		lbl.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		box.add(lbl);
+		
+		try
+		{
+			lbl = new JLabel("Ladedauer: " + Integer.parseInt(Controller.getInstance().getData().readSetting("LastLoadTime")) / 1000d + "Sekunden");
+			lbl.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+			box.add(lbl);
+		}catch (SettingException e){}
 
 
-				add(box);
-			}});
+		add(box);
 	}
 }
