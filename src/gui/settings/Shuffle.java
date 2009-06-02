bf1fc93c83e7a03083191bfe8c9c7d57cd4b5fcb
Eraser <Eraser@0e6492ef-cedd-114b-947f-3769be7ed64a>
2009-06-02 13:57:22 +0000

    git-svn-id: file:///e/Backups/vorderklier.de/backup/160817/data/svn/PartyDJ_Alt@312 0e6492ef-cedd-114b-947f-3769be7ed64a

--- src/gui/settings/Shuffle.java
+++ src/gui/settings/Shuffle.java
@@ -371,13 +371,6 @@ public class Shuffle extends JPanel
 
 		//======== this ========
 
-		// JFormDesigner evaluation mark
-		setBorder(new javax.swing.border.CompoundBorder(
-			new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-				"JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
-				javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-				java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
-
 		setLayout(new FormLayout(
 			"$rgap, $lcgap, 323dlu:grow, $lcgap, 2dlu",
 			"fill:23dlu, $lgap, fill:18dlu, 16dlu, fill:64dlu:grow, $lgap, $nlgap"));
