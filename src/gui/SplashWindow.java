2b6718d320a8ba41ca9e02745cb59ffb7893934f
Eraser <Eraser@0e6492ef-cedd-114b-947f-3769be7ed64a>
2009-06-02 10:02:47 +0000

    Mit Refection läufts auch unter 1.6.0
    git-svn-id: file:///e/Backups/vorderklier.de/backup/160817/data/svn/PartyDJ_Alt@304 0e6492ef-cedd-114b-947f-3769be7ed64a

--- src/gui/SplashWindow.java
+++ src/gui/SplashWindow.java
@@ -3,7 +3,10 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Shape;
+import java.awt.Window;
 import java.awt.geom.RoundRectangle2D;
+import java.lang.reflect.Method;
 import java.util.Timer;
 import java.util.TimerTask;
 import javax.swing.ImageIcon;
@@ -22,64 +25,66 @@ import javax.swing.SwingUtilities;
 public class SplashWindow extends JWindow
 {
 	private static final long serialVersionUID = 2036905786871782351L;
-	private SplashWindow me = this;
 	private JLabel info;
 	private JLabel timer;
 	private long startTime;
 
 	public SplashWindow()
 	{
-		SwingUtilities.invokeLater(new Runnable(){
-			@Override
-			public void run()
-			{
-				startTime = System.currentTimeMillis();
+		startTime = System.currentTimeMillis();
 
-				setLayout(new BorderLayout());
-				getContentPane().setBackground(Color.DARK_GRAY);
-				setSize(757, 321 + 32);
-				setLocationRelativeTo(null);
-				
-				JLabel picture = null;
-				try
-				{
-					picture = new JLabel(new ImageIcon("Resources/Schriftzug.png"));
-				}
-				catch (NullPointerException e)
-				{
-					picture = new JLabel("Hier fehlt ein Bild :(");
-				}
-				picture.setHorizontalAlignment(JLabel.CENTER);
-				
-				info = new JLabel("Lade PartyDJ");
-				info.setFont(new Font(Font.SERIF, 0, 24));
-				info.setForeground(Color.GREEN);
-				info.setHorizontalAlignment(JLabel.CENTER);
-				
-				timer = new JLabel("0");
-				timer.setFont(new Font(Font.SERIF, 0, 10));
-				timer.setVerticalAlignment(JLabel.BOTTOM);
-				timer.setForeground(Color.GREEN);
-				timer.setHorizontalAlignment(JLabel.LEFT);
-				
-				JPanel panel = new JPanel();
-				panel.setLayout(new BorderLayout());
-				panel.add(info, BorderLayout.CENTER);
-				panel.add(timer, BorderLayout.EAST);
-				panel.setBackground(getBackground());
-				
-				add(picture, BorderLayout.CENTER);
-				add(panel, BorderLayout.SOUTH);
-				
-				new Timer().schedule(new UpdateTask(), 0, 40);
+		setLayout(new BorderLayout());
+		getContentPane().setBackground(Color.DARK_GRAY);
+		setSize(757, 321 + 32);
+		setLocationRelativeTo(null);
+		
+		JLabel picture = null;
+		try
+		{
+			picture = new JLabel(new ImageIcon("Resources/Schriftzug.png"));
+		}
+		catch (NullPointerException e)
+		{
+			picture = new JLabel("Hier fehlt ein Bild :(");
+		}
+		picture.setHorizontalAlignment(JLabel.CENTER);
+		
+		info = new JLabel("Lade PartyDJ");
+		info.setFont(new Font(Font.SERIF, 0, 24));
+		info.setForeground(Color.GREEN);
+		info.setHorizontalAlignment(JLabel.CENTER);
+		
+		timer = new JLabel("0");
+		timer.setFont(new Font(Font.SERIF, 0, 10));
+		timer.setVerticalAlignment(JLabel.BOTTOM);
+		timer.setForeground(Color.GREEN);
+		timer.setHorizontalAlignment(JLabel.LEFT);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.add(info, BorderLayout.CENTER);
+		panel.add(timer, BorderLayout.EAST);
+		panel.setBackground(getBackground());
+		
+		add(picture, BorderLayout.CENTER);
+		add(panel, BorderLayout.SOUTH);
+		
+		new Timer().schedule(new UpdateTask(), 0, 40);
 
-				
-				// Ecken Abrunden
-				com.sun.awt.AWTUtilities.setWindowShape(me, new RoundRectangle2D.Double(0, 0, me.getWidth(), me.getHeight(), 20, 20));
+		
+		// Ecken Abrunden
+		//com.sun.awt.AWTUtilities.setWindowShape(me, new RoundRectangle2D.Double(0, 0, me.getWidth(), me.getHeight(), 20, 20));				
+		try 
+		{
+			Class<?> utils = Class.forName("com.sun.awt.AWTUtilities");
+			Method method = utils.getMethod("setWindowShape", Window.class, Shape.class);
+			Shape s = new RoundRectangle2D.Double(0, 0, this.getWidth(), this.getHeight(), 20, 20);
+			method.invoke(this, this, s);
+		}
+		catch(Exception ignored){}
 
-				setOpacity(.8f);
-				setVisible(true);
-			}});
+		setOpacity(.8f);
+		setVisible(true);
 	}
 	
 	public void setInfo(final String infoString)
@@ -100,7 +105,14 @@ public class SplashWindow extends JWindow
 			@Override
 			public void run()
 			{
-				com.sun.awt.AWTUtilities.setWindowOpacity(me, opacity);
+				//com.sun.awt.AWTUtilities.setWindowOpacity(me, opacity);
+				try 
+				{
+					Class<?> utils = Class.forName("com.sun.awt.AWTUtilities");
+					Method method = utils.getMethod("setWindowOpacity", Window.class, float.class);
+					method.invoke(this, SplashWindow.this, opacity);
+				}
+				catch(Exception ignored){}
 			}});
 		
 	}
