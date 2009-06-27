9979d2e2862d8f84b572fd9905fee5d6b74f23f5
Eraser <Eraser@0e6492ef-cedd-114b-947f-3769be7ed64a>
2009-06-27 20:51:13 +0000

    Wacken-Liste
    git-svn-id: file:///e/Backups/vorderklier.de/backup/160817/data/svn/PartyDJ_Alt@360 0e6492ef-cedd-114b-947f-3769be7ed64a

---
+++ src/gui/ListWindow.java
@@ -0,0 +1,20 @@
+package gui;
+
+import javax.swing.JFrame;
+import lists.ListException;
+import basics.Controller;
+
+public class ListWindow extends JFrame
+{
+	private static final long serialVersionUID = 3149870698027861160L;
+	protected final PDJScrollList list;
+	
+	public ListWindow(String listName) throws ListException
+	{
+		list = new PDJScrollList(Controller.getInstance().getListProvider().getDbList(listName));
+		add(list);
+		setSize(800, 600);
+		setTitle(listName);
+		setVisible(true);
+	}
+}
--- src/gui/settings/Stuff.java
+++ src/gui/settings/Stuff.java
@@ -1,5 +1,7 @@
 package gui.settings;
 
+import gui.ClassicWindow;
+import gui.ListWindow;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,7 +24,10 @@ import basics.Controller;
 public class Stuff extends javax.swing.JPanel {
 	private static final long serialVersionUID = 5155888057108134828L;
 	private JButton btnDerbyDebug;
-	private JButton btsSwap;
+	private JButton btnWacken;
+	private JButton btnClassicWindow;
+	
+	protected final Controller controller = Controller.getInstance();
 
 	public Stuff() {
 		super();
@@ -36,55 +41,66 @@ public class Stuff extends javax.swing.JPanel {
 		
 	}
 	
-	private void initGUI() {
-		try {
-			GroupLayout thisLayout = new GroupLayout(this);
-			this.setLayout(thisLayout);
-			setPreferredSize(new Dimension(400, 300));
-			{
-				btnDerbyDebug = new JButton();
-				btnDerbyDebug.setText("Datenbank Debug-Fenster ...");
-				btnDerbyDebug.addActionListener(new ActionListener(){
-					public void actionPerformed(ActionEvent e)
-					{
-						Controller.getInstance().registerWindow(new data.derby.DebugWindow());
-					}});
-			}
-			{
-				btsSwap = new JButton();
-				btsSwap.setText("Swap Test");
-				btsSwap.addActionListener(new ActionListener(){
-					public void actionPerformed(ActionEvent e)
-					{
-						try
-						{
-							Controller.getInstance().getListProvider().getDbList("Playlist").swap(1, 4);
-						}
-						catch (ListException e1)
-						{
-							e1.printStackTrace();
-						}
-					}});
-			}
-			thisLayout.setVerticalGroup(thisLayout.createSequentialGroup()
-				.addContainerGap()
-				.addComponent(btnDerbyDebug, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
-				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-				.addComponent(btsSwap, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-				.addContainerGap(233, Short.MAX_VALUE));
-			thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup()
-				.addContainerGap()
-				.addGroup(thisLayout.createParallelGroup()
-				    .addGroup(thisLayout.createSequentialGroup()
-				        .addComponent(btnDerbyDebug, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
-				        .addGap(0, 0, Short.MAX_VALUE))
-				    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
-				        .addComponent(btsSwap, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-				        .addGap(0, 105, Short.MAX_VALUE)))
-				.addContainerGap(224, 224));
-		} catch (Exception e) {
-			e.printStackTrace();
+	private void initGUI()
+	{
+		GroupLayout thisLayout = new GroupLayout(this);
+		this.setLayout(thisLayout);
+		setPreferredSize(new Dimension(400, 300));
+		{
+			btnDerbyDebug = new JButton();
+			btnDerbyDebug.setText("Datenbank Debug-Fenster ...");
+			btnDerbyDebug.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e)
+				{
+					controller.registerWindow(new data.derby.DebugWindow());
+				}});
 		}
+		{
+			btnClassicWindow = new JButton();
+			btnClassicWindow.setText("Classic Window");
+			btnClassicWindow.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent evt) {
+					controller.registerWindow(new ClassicWindow());
+				}
+			});
+		}
+		{
+			btnWacken = new JButton();
+			btnWacken.setText("Wacken Liste");
+			btnWacken.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent evt) {
+					try
+					{
+						Controller.getInstance().registerWindow(new ListWindow("Wacken"));
+					}
+					catch (ListException e)
+					{
+						controller.logError(Controller.NORMAL_ERROR, this, e, "Liste konnte nicht erstellt werden.");
+					}
+				}
+			});
+		}
+		thisLayout.setVerticalGroup(thisLayout.createSequentialGroup()
+			.addContainerGap()
+			.addComponent(btnDerbyDebug, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+			.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+			.addComponent(btnClassicWindow, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+			.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+			.addComponent(btnWacken, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+			.addContainerGap(200, Short.MAX_VALUE));
+		thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup()
+			.addContainerGap()
+			.addGroup(thisLayout.createParallelGroup()
+			    .addGroup(thisLayout.createSequentialGroup()
+			        .addComponent(btnDerbyDebug, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+			        .addGap(0, 0, Short.MAX_VALUE))
+			    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+			        .addComponent(btnClassicWindow, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+			        .addGap(0, 67, Short.MAX_VALUE))
+			    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+			        .addComponent(btnWacken, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+			        .addGap(0, 105, Short.MAX_VALUE)))
+			.addContainerGap(224, 224));
 	}
 
 }
